ea6561e51390c11332ae49e57072852b8d2fcfeb
Eliska Suchardova <eliska.suchardova@gmail.com>
2025-03-22 10:32:25 +0100

    calculator

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -5,8 +5,9 @@ import { toPrompt } from '@/lib/prompt'
 import ratelimit from '@/lib/ratelimit'
 import { fragmentSchema as schema } from '@/lib/schema'
 import { Templates } from '@/lib/templates'
-import { streamObject, LanguageModel, CoreMessage } from 'ai'
+import { streamObject, LanguageModel, CoreMessage, experimental_createMCPClient, generateText } from 'ai'
 import { Experimental_StdioMCPTransport } from 'ai/mcp-stdio';
+import { openai } from '@ai-sdk/openai';
 
 
 export const maxDuration = 60
@@ -33,41 +34,91 @@ export async function POST(req: Request) {
     config: LLMModelConfig
   } = await req.json()
 
-  const limit = !config.apiKey
-    ? await ratelimit(
-        userID,
-        rateLimitMaxRequests,
-        ratelimitWindow,
-      )
-    : false
+//  const limit = !config.apiKey
+//    ? await ratelimit(
+//        userID,
+//        rateLimitMaxRequests,
+//        ratelimitWindow,
+//      )
+//    : false
+//
+//  if (limit) {
+//    return new Response('You have reached your request limit for the day.', {
+//      status: 429,
+//      headers: {
+//        'X-RateLimit-Limit': limit.amount.toString(),
+//        'X-RateLimit-Remaining': limit.remaining.toString(),
+//        'X-RateLimit-Reset': limit.reset.toString(),
+//      },
+//    })
+//  }
+//
+//  console.log('userID', userID)
+//  // console.log('template', template)
+//  console.log('model', model)
+//  // console.log('config', config)
+//
+//  const { model: modelNameString, apiKey: modelApiKey, ...modelParams } = config
+//  const modelClient = getModelClient(model, config)
+//
+//  const stream = await streamObject({
+//    model: modelClient as LanguageModel,
+//    schema,
+//    system: toPrompt(template),
+//    messages,
+//    mode: getDefaultMode(model),
+//    ...modelParams,
+//  })
+//
+//  return stream.toTextStreamResponse()
+let clientOne : any;
+try {
+    // Initialize an MCP client to connect to a `stdio` MCP server:
+    const transport = new Experimental_StdioMCPTransport({
+      command: 'node',
+      args: ['e2b-hackathon/app/api/servers/stdio/calculator.js'],
+    });
+    clientOne = await experimental_createMCPClient({
+      transport,
+    });
 
-  if (limit) {
-    return new Response('You have reached your request limit for the day.', {
-      status: 429,
-      headers: {
-        'X-RateLimit-Limit': limit.amount.toString(),
-        'X-RateLimit-Remaining': limit.remaining.toString(),
-        'X-RateLimit-Reset': limit.reset.toString(),
-      },
-    })
+    // Alternatively, you can connect to a Server-Sent Events (SSE) MCP server:
+    //clientTwo = await experimental_createMCPClient({
+    //  transport: {
+    //    type: 'sse',
+    //    url: 'http://localhost:3000/sse',
+    //  },
+    //});
+
+    // Similarly to the stdio example, you can pass in your own custom transport as long as it implements the `MCPTransport` interface:
+    //const transport = new MyCustomTransport({
+    //  // ...
+    //});
+    //clientThree = await experimental_createMCPClient({
+    //  transport,
+    //});
+
+    const toolSetOne = await clientOne.tools();
+    //const toolSetTwo = await clientTwo.tools();
+    //const toolSetThree = await clientThree.tools();
+    const tools = {
+      ...toolSetOne,
+      //...toolSetTwo,
+      //...toolSetThree, // note: this approach causes subsequent tool sets to override tools with the same name
+    };
+
+    const response = await generateText({
+      model: openai('gpt-4o'),
+      tools,
+      messages: messages
+    });
+
+    console.log(response.text);
+    return response;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await Promise.all([clientOne.close()]);
   }
-
-  console.log('userID', userID)
-  // console.log('template', template)
-  console.log('model', model)
-  // console.log('config', config)
-
-  const { model: modelNameString, apiKey: modelApiKey, ...modelParams } = config
-  const modelClient = getModelClient(model, config)
-
-  const stream = await streamObject({
-    model: modelClient as LanguageModel,
-    schema,
-    system: toPrompt(template),
-    messages,
-    mode: getDefaultMode(model),
-    ...modelParams,
-  })
-
-  return stream.toTextStreamResponse()
 }
