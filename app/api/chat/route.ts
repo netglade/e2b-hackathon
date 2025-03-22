78291ae352edd2caec01020e9636c5628ecbc220
Michal Ivičič <ivicic@netglade.cz>
2025-03-22 11:32:24 +0100

    * print results

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,14 +1,9 @@
 import { Duration } from '@/lib/duration'
-import { getModelClient, getDefaultMode } from '@/lib/models'
 import { LLMModel, LLMModelConfig } from '@/lib/models'
-import { toPrompt } from '@/lib/prompt'
-import ratelimit from '@/lib/ratelimit'
-import { fragmentSchema as schema } from '@/lib/schema'
 import { Templates } from '@/lib/templates'
-import { streamObject, LanguageModel, CoreMessage, experimental_createMCPClient, generateText } from 'ai'
-import { Experimental_StdioMCPTransport } from 'ai/mcp-stdio';
-import { openai } from '@ai-sdk/openai';
-
+import { openai } from '@ai-sdk/openai'
+import { CoreMessage, experimental_createMCPClient, generateText } from 'ai'
+import { Experimental_StdioMCPTransport } from 'ai/mcp-stdio'
 
 export const maxDuration = 60
 
@@ -34,53 +29,53 @@ export async function POST(req: Request) {
     config: LLMModelConfig
   } = await req.json()
 
-//  const limit = !config.apiKey
-//    ? await ratelimit(
-//        userID,
-//        rateLimitMaxRequests,
-//        ratelimitWindow,
-//      )
-//    : false
-//
-//  if (limit) {
-//    return new Response('You have reached your request limit for the day.', {
-//      status: 429,
-//      headers: {
-//        'X-RateLimit-Limit': limit.amount.toString(),
-//        'X-RateLimit-Remaining': limit.remaining.toString(),
-//        'X-RateLimit-Reset': limit.reset.toString(),
-//      },
-//    })
-//  }
-//
-//  console.log('userID', userID)
-//  // console.log('template', template)
-//  console.log('model', model)
-//  // console.log('config', config)
-//
-//  const { model: modelNameString, apiKey: modelApiKey, ...modelParams } = config
-//  const modelClient = getModelClient(model, config)
-//
-//  const stream = await streamObject({
-//    model: modelClient as LanguageModel,
-//    schema,
-//    system: toPrompt(template),
-//    messages,
-//    mode: getDefaultMode(model),
-//    ...modelParams,
-//  })
-//
-//  return stream.toTextStreamResponse()
-let clientOne : any;
-try {
+  //  const limit = !config.apiKey
+  //    ? await ratelimit(
+  //        userID,
+  //        rateLimitMaxRequests,
+  //        ratelimitWindow,
+  //      )
+  //    : false
+  //
+  //  if (limit) {
+  //    return new Response('You have reached your request limit for the day.', {
+  //      status: 429,
+  //      headers: {
+  //        'X-RateLimit-Limit': limit.amount.toString(),
+  //        'X-RateLimit-Remaining': limit.remaining.toString(),
+  //        'X-RateLimit-Reset': limit.reset.toString(),
+  //      },
+  //    })
+  //  }
+  //
+  //  console.log('userID', userID)
+  //  // console.log('template', template)
+  //  console.log('model', model)
+  //  // console.log('config', config)
+  //
+  //  const { model: modelNameString, apiKey: modelApiKey, ...modelParams } = config
+  //  const modelClient = getModelClient(model, config)
+  //
+  //  const stream = await streamObject({
+  //    model: modelClient as LanguageModel,
+  //    schema,
+  //    system: toPrompt(template),
+  //    messages,
+  //    mode: getDefaultMode(model),
+  //    ...modelParams,
+  //  })
+  //
+  //  return stream.toTextStreamResponse()
+  let clientOne: any
+  try {
     // Initialize an MCP client to connect to a `stdio` MCP server:
     const transport = new Experimental_StdioMCPTransport({
       command: 'node',
       args: ['./app/api/servers/stdio/calculator.js'],
-    });
+    })
     clientOne = await experimental_createMCPClient({
       transport,
-    });
+    })
 
     // Alternatively, you can connect to a Server-Sent Events (SSE) MCP server:
     //clientTwo = await experimental_createMCPClient({
@@ -98,38 +93,43 @@ try {
     //  transport,
     //});
 
-    const toolSetOne = await clientOne.tools();
+    const toolSetOne = await clientOne.tools()
     //const toolSetTwo = await clientTwo.tools();
     //const toolSetThree = await clientThree.tools();
     const tools = {
       ...toolSetOne,
       //...toolSetTwo,
       //...toolSetThree, // note: this approach causes subsequent tool sets to override tools with the same name
-    };
+    }
 
     const response = await generateText({
       model: openai('gpt-4o'),
       tools,
-      messages: messages
-    });
+      maxSteps: 10,
+      messages,
+    })
 
-    console.log(response);
+    console.log(JSON.stringify(response.response.messages))
+    console.log(response.text)
     return new Response(JSON.stringify(response), {
       headers: {
         'Content-Type': 'application/json',
       },
-    });
+    })
   } catch (error) {
-    console.error(error);
-    return new Response(JSON.stringify({ error: 'Failed to process request' }), {
-      status: 500,
-      headers: {
-        'Content-Type': 'application/json',
+    console.error(error)
+    return new Response(
+      JSON.stringify({ error: 'Failed to process request' }),
+      {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
       },
-    });
+    )
   } finally {
     if (clientOne) {
-      await clientOne.close();
+      await clientOne.close()
     }
   }
 }
